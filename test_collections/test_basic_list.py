d8144864a217f4a7bdf6c10527f93e401e7aad34
Omar <gomar@dmacc.edu>
2019-10-07 13:11:02 -0500

    Test passed

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -5,9 +5,9 @@ import fun_with_collections.basic_list as basic_list
 
 class MyTestCase(unittest.TestCase):
  def test_make_list(self):
-   with patch('fun_with_collections.basic_list.get_input', return_value='8'):
+   with patch('fun_with_collections.basic_list.get_input', return_value='9'):
     def test_make_list(self, input):
-        self.assertEqual(basic_list.make_list(input), [6, 8, 5])
+        self.assertEqual(basic_list.make_list(input), [8, 6, 9])
 
 
 if __name__ == '__main__':
